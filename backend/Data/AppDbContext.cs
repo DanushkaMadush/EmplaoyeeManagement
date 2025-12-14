fb46217ba8fb9148a3894e3656b919a88220ef1e
danushka <danmadushzxc@gmail.com>
2025-12-14 22:10:16 +0530

    feat:added databse connection

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace backend.Data
+{
+    public class AppDbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
