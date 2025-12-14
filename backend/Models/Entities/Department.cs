4b77aad09afa824a0e55e8e40cdc284c8bf5bdea
danushka <danmadushzxc@gmail.com>
2025-12-14 22:10:42 +0530

    feat:added entities

---
+++ backend/Models/Entities/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models.Entities
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public required string DepartmentCode { get; set; }
+        public required string DepartmentName { get; set; }
+    }
+}
---
+++ backend/Models/Entities/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace backend.Models.Entities
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public required string FirstName { get; set; }
+        public string? LastName { get; set; } = null;
+        public required string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public decimal Salary { get; set; }
+        public int DepartmentId { get; set; }
+
+        //calculate age from date of birth
+        public int Age
+        {
+            get
+            {
+                var today = DateTime.Today;
+                var age = today.Year - DateOfBirth.Year;
+                if (DateOfBirth.Date > today.AddYears(-age)) age--;
+                return age;
+            }
+        }
+    }
+}
