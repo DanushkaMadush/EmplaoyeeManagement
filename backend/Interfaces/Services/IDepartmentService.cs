8afff746d647111425292f41ecf43b8e220a2878
danushka <danmadushzxc@gmail.com>
2025-12-15 21:08:40 +0530

    faet:implemented interfaces for services

---
+++ backend/Interfaces/Services/IDepartmentService.cs
@@ -0,0 +1,13 @@
+﻿using backend.Models.Entities;
+
+namespace backend.Interfaces.Services
+{
+    public interface IDepartmentService
+    {
+        Task<IEnumerable<Department>> GetAllAsync();
+        Task<Department?> GetByIdAsync(int id);
+        Task<int> CreateAsync(Department department);
+        Task<bool> UpdateAsync(Department department);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ backend/Interfaces/Services/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using backend.Models.Entities;
+
+namespace backend.Interfaces.Services
+{
+    public interface IEmployeeService
+    {
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task<Employee?> GetByIdAsync(int id);
+        Task<IEnumerable<Employee>> GetByDepartmentAsync(int departmentId);
+        Task<int> CreateAsync(Employee employee);
+        Task<bool> UpdateAsync(Employee employee);
+        Task<bool> DeleteAsync(int id);
+    }
+}
