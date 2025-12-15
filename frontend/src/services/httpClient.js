c39910015cf893a7011a59ae0a576220bbc2470e
danushka <danmadushzxc@gmail.com>
2025-12-15 22:11:54 +0530

    refactor:changed baseurl

--- frontend/src/services/httpClient.js
+++ frontend/src/services/httpClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-  baseURL: 'https://localhost:3000/api',
+  baseURL: 'https://localhost:7090/api',
   headers: {
     'Content-Type': 'application/json'
   }
