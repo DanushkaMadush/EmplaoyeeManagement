891079d4c2fecda94a2a8b07db65a4269e950011
danushka <danmadushzxc@gmail.com>
2025-12-15 21:52:11 +0530

    feat:cors allowed from all resources

--- backend/Program.cs
+++ backend/Program.cs
@@ -8,6 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 
 builder.Services.AddScoped<AppDbContext>();
 
@@ -32,6 +42,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
