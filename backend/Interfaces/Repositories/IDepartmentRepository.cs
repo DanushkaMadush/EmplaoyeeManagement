0a5da34f852e56d1dc200a15129dddf94b9b077e
danushka <danmadushzxc@gmail.com>
2025-12-15 20:59:37 +0530

    feat:implemeted repository interfaces

---
+++ backend/Interfaces/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using backend.Models.Entities;
+
+namespace backend.Interfaces.Repositories
+{
+    public interface IDepartmentRepository
+    {
+        Task<IEnumerable<Department>> GetAllAsync();
+        Task<Department?> GetByIdAsync(int departmentId);
+        Task<Department?> GetByCodeAsync(string departmentCode);
+        Task<int> CreateAsync(Department department);
+        Task<bool> UpdateAsync(Department department);
+        Task<bool> DeleteAsync(int departmentId);
+    }
+}
---
+++ backend/Interfaces/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using backend.Models.Entities;
+
+namespace backend.Interfaces.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task<Employee?> GetByIdAsync(int employeeId);
+        Task<IEnumerable<Employee>> GetByDepartmentAsync(int departmentId);
+        Task<int> CreateAsync(Employee employee);
+        Task<bool> UpdateAsync(Employee employee);
+        Task<bool> DeleteAsync(int employeeId);
+    }
+}
